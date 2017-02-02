c8d391bfdac33d43ad6ce868308e4ee96fa11c9d
jeferbc <jeffe.bernal@gmail.com>
2017-02-01 22:19:17 -0500

    detect key arrows

--- main.rb
+++ main.rb
@@ -1,9 +1,43 @@
+require 'io/console'
+
 class Game
   def self.generate_puzzle
     array = (1..8).to_a
     array << " "
     array.shuffle
   end
+  def self.move(direction)
+
+  end
+end
+
+# Reads keypresses from the user including 2 and 3 escape character sequences.
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
 end
 
 puts Game.generate_puzzle
+move = read_char
+puts move.inspect
+case move
+  when "\e[A"
+    puts "UP ARROW"
+  when "\e[B"
+    puts "DOWN ARROW"
+  when "\e[C"
+    puts "RIGHT ARROW"
+  when "\e[D"
+    puts "LEFT ARROW"
+end
